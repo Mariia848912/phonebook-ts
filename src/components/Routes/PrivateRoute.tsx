c9cc47ac9a71188ec43965fe3d419411e4ac00ff
Mariia Sarbieieva <sarbeeva.m.a@gmail.com>
2023-11-11 13:53:23 +0200

    chore

--- src/components/Routes/PrivateRoute.tsx
+++ src/components/Routes/PrivateRoute.tsx
@@ -1,9 +1,12 @@
 import { useAuth } from "../../hooks";
-import { Navigate } from 'react-router-dom';
+import { Navigate } from "react-router-dom";
 import { RouteProps } from "./routes.types";
 
-export const PrivateRoute = ({ component: Component, redirectTo = '/' }:RouteProps) => {
-      const { isLoggedIn, isRefreshing } = useAuth();
-    const shouldRedirect = !isLoggedIn && !isRefreshing;
-    return shouldRedirect ? <Navigate to={redirectTo} /> : Component;
-}
+export const PrivateRoute = ({
+  component: Component,
+  redirectTo = "/",
+}: RouteProps) => {
+  const { isLoggedIn, isRefreshing } = useAuth();
+  const shouldRedirect = !isLoggedIn && !isRefreshing;
+  return shouldRedirect ? <Navigate to={redirectTo} /> : Component;
+};
--- src/components/Routes/RestrictedRoute.tsx
+++ src/components/Routes/RestrictedRoute.tsx
@@ -1,8 +1,11 @@
 import { useAuth } from "../../hooks";
-import { Navigate } from 'react-router-dom';
+import { Navigate } from "react-router-dom";
 import { RouteProps } from "./routes.types";
 
-export const RestrictedRoute = ({ component: Component, redirectTo = '/' }:RouteProps) => {
+export const RestrictedRoute = ({
+  component: Component,
+  redirectTo = "/",
+}: RouteProps) => {
   const { isLoggedIn } = useAuth();
 
   return isLoggedIn ? <Navigate to={redirectTo} /> : Component;
